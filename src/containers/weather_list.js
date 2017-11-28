06c7ee4138ebe8c726d7b9ff279126284568e2a7
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-11-27 22:36:43 -0800

    Added funcionality to SearchBar container, created and added WeatherList container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class WeatherList extends Component {
+    render() {
+        return (
+            <table className='table table-hover'>
+                <thead>
+                    <th>City</th>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        );
+    }
+}
